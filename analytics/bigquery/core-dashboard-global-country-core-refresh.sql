47703ecccc3fdd678a96150519840b2ac5a9e2fa
cyanlee888 <cyanlee888@gmail.com>
2026-08-24 11:24:45 +0800

    刷新 8 月 24 日核心数据看板

--- analytics/bigquery/core-dashboard-global-country-core-refresh.sql
+++ analytics/bigquery/core-dashboard-global-country-core-refresh.sql
@@ -2,7 +2,7 @@
 -- Country for an account is the geo.country of its earliest mapped GA4 device event
 -- in the reporting window. First-open funnel/retention use first_open country separately.
 
-DECLARE cutoff TIMESTAMP DEFAULT TIMESTAMP '2026-08-17 02:15:00+00';
+DECLARE cutoff TIMESTAMP DEFAULT TIMESTAMP '2026-08-24 03:01:17+00';
 
 WITH periods AS (
   SELECT * FROM UNNEST([
@@ -12,7 +12,8 @@ WITH periods AS (
     ('w3', TIMESTAMP '2026-07-24 00:00:00+00', TIMESTAMP '2026-07-31 00:00:00+00'),
     ('w4', TIMESTAMP '2026-07-31 00:00:00+00', TIMESTAMP '2026-08-07 00:00:00+00'),
     ('w5', TIMESTAMP '2026-08-07 00:00:00+00', TIMESTAMP '2026-08-14 00:00:00+00'),
-    ('w6', TIMESTAMP '2026-08-14 00:00:00+00', cutoff)
+    ('w6', TIMESTAMP '2026-08-14 00:00:00+00', TIMESTAMP '2026-08-21 00:00:00+00'),
+    ('w7', TIMESTAMP '2026-08-21 00:00:00+00', cutoff)
   ])
 ),
 country_targets AS (
@@ -22,11 +23,11 @@ raw_base AS (
   SELECT event_timestamp, event_name, user_pseudo_id, user_id, platform, geo.country AS country, event_params, user_properties
   FROM `dino-english-497507.analytics_538991439.events_*`
   WHERE REGEXP_CONTAINS(_TABLE_SUFFIX, r'^[0-9]{8}$')
-    AND _TABLE_SUFFIX BETWEEN '20260710' AND '20260815'
+    AND _TABLE_SUFFIX BETWEEN '20260710' AND '20260822'
   UNION ALL
   SELECT event_timestamp, event_name, user_pseudo_id, user_id, platform, geo.country AS country, event_params, user_properties
   FROM `dino-english-497507.analytics_538991439.events_intraday_*`
-  WHERE _TABLE_SUFFIX BETWEEN '20260816' AND '20260817'
+  WHERE _TABLE_SUFFIX BETWEEN '20260823' AND '20260824'
 ),
 test_devices AS (
   SELECT DISTINCT platform, user_pseudo_id
